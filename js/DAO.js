1c437018bc92fe7ac188ce7ca61f60296ed8ba89
Nick Ferraro <spidy.88@gmail.com>
2014-04-06 15:13:26 -0700

    Added action click listener for action to grid component Added card flip animation Updated some styles

--- js/DAO.js
+++ js/DAO.js
@@ -2,24 +2,139 @@
 
 exports.getActions = function() {
     var items = [{
-        title : "Action 1",
-        icon : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png"
-    }, {
-        title : "Action 2",
-        icon : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Baidu-icon.png"
-    }, {
-        title : "Action 3",
-        icon : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Dzone-icon.png"
-    }, {
-        title : "Action 4",
-        icon : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Formspring-icon.png"
-    }, {
-        title : "Action 5",
-        icon : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Foursquare-icon.png"
-    }, {
-        title : "Action 6",
-        icon : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Evernote-icon.png"
-    }];
-
+		"name" : "LogACall",
+		"type" : "LogACall",
+		"label" : "Log a Call",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/LogACall/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/LogACall",
+			"defaultValues" : "/services/data/v29.0/quickActions/LogACall/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/LogACall/describe"
+		}
+		}, {
+		"name" : "NewAccount",
+		"type" : "Create",
+		"label" : "New Account",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewAccount/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewAccount",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewAccount/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewAccount/describe"
+		}
+	}, {
+		"name" : "NewCase",
+		"type" : "Create",
+		"label" : "New Case",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewCase/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewCase",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewCase/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewCase/describe"
+		}
+	}, {
+		"name" : "NewContact",
+		"type" : "Create",
+		"label" : "New Contact",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewContact/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewContact",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewContact/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewContact/describe"
+		}
+	}, {
+		"name" : "NewEvent",
+		"type" : "Create",
+		"label" : "New Event",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewEvent/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewEvent",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewEvent/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewEvent/describe"
+		}
+	}, {
+		"name" : "NewLead",
+		"type" : "Create",
+		"label" : "New Lead",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewLead/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewLead",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewLead/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewLead/describe"
+		}
+	}, {
+		"name" : "NewNote",
+		"type" : "Create",
+		"label" : "New Note",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewNote/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewNote",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewNote/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewNote/describe"
+		}
+	}, {
+		"name" : "NewOpportunity",
+		"type" : "Create",
+		"label" : "New Opportunity",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewOpportunity/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewOpportunity",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewOpportunity/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewOpportunity/describe"
+		}
+	}, {
+		"name" : "NewTask",
+		"type" : "Create",
+		"label" : "New Task",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+			"defaultValuesTemplate" : "/services/data/v29.0/quickActions/NewTask/defaultValues/{ID}",
+			"quickAction" : "/services/data/v29.0/quickActions/NewTask",
+			"defaultValues" : "/services/data/v29.0/quickActions/NewTask/defaultValues",
+			"describe" : "/services/data/v29.0/quickActions/NewTask/describe"
+		}
+	}, {
+		"name" : "FeedItem.TextPost",
+		"type" : "Post",
+		"label" : "Post",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+		}
+	}, {
+		"name" : "FeedItem.ContentPost",
+		"type" : "Post",
+		"label" : "File",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+		}
+	}, {
+		"name" : "FeedItem.MobileSmartActions",
+		"type" : "Post",
+		"label" : "Mobile Smart Actions",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+		}
+	}, {
+		"name" : "FeedItem.LinkPost",
+		"type" : "Post",
+		"label" : "Link",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+		}
+	}, {
+		"name" : "FeedItem.PollPost",
+		"type" : "Post",
+		"label" : "Poll",
+		"icon" : "http://icons.iconarchive.com/icons/designbolts/handstitch-social/128/Android-icon.png",
+		"urls" : {
+		}
+	}];
     return items;
 }
