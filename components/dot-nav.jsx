614c941b07f7401e78006c9605da726a04ca56dc
Nick Ferraro <spidy.88@gmail.com>
2014-04-23 19:50:03 -0700

    Added dynamic dot number with active class

--- components/dot-nav.jsx
+++ components/dot-nav.jsx
@@ -5,16 +5,32 @@
 var React = require("react");
 
 module.exports = React.createClass({
+	propTypes: {
+		dots: React.PropTypes.number.isRequired
+	},
+	getDefaultProps: function() {
+		return {
+			dots: 4,
+			maxDots: 6
+		};
+	},
+	getInitialState: function() {
+		return {
+			page: 0
+		}
+	},
     render: function() {
+    	var pages = Math.min(this.props.dots, this.props.maxDots);
+		var dots = [];
+		
+		for( var i = 0; i < this.props.dots; ++i ) {
+			dots.push(<li className={ i == this.state.page ? "active" : "" }><a href="#"></a></li>);
+		}
+		
         return (
         	<div className="page-nav">
         		<ul>
-        			<li className="active"><a href="#"></a></li>
-        			<li><a href="#"></a></li>
-        			<li><a href="#"></a></li>
-        			<li><a href="#"></a></li>
-        			<li><a href="#"></a></li>
-        			<li><a href="#"></a></li>
+        			{ dots }
         		</ul>
         	</div>
         );
